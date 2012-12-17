492776984230ef726de4be5e69f8b993234d9b03
Andrea Campi <andrea.campi@zephirworks.com>
2012-12-17 23:43:51 +0100

    Tests that require a user to exist must create it.

--- test/kitchen/cookbooks/cloudfoundry_test/recipes/default.rb
+++ test/kitchen/cookbooks/cloudfoundry_test/recipes/default.rb
@@ -16,3 +16,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+group "cloudfoundry" do
+  gid 10000
+end
+
+user "cloudfoundry" do
+  uid 10000
+  gid 10000
+end
